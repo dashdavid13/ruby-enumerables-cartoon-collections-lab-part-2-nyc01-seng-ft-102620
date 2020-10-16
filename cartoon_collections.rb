164e23ec59f8a48655e19610f2a00a1da8172dfa
David Medina <davidamedina619@gmail.com>
2020-10-16 21:44:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,7 +34,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
    planeteer_calls.find do |element|
-     planeteer_calls.include?(type)
+     planeteer_calls.include?(element)
       
   end 
 end
