e1bbad9c6024ba6fe5481d0057305475711e9ffc
David Medina <davidamedina619@gmail.com>
2020-10-16 21:26:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,15 @@ def square_array(array)
   end 
   new_array
 end 
-
-def summon_captain_planet(planeteer_calls)
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
+ # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+  
+def summon_captain_planet(planeteer_calls)
+ new_array =[]
+  planeteer_calls.each do |index|
+   new_array << index.capitalize && index.push("!")
+  end 
+  new_array
 end
 
 def long_planeteer_calls(planeteer_calls)
